562154a6fa3a48ad4cc8c06e585dd9c9a529f1e4
ElementalistBTG <elementalistbtg@gmail.com>
2022-05-25 11:39:32 +0300

    testing versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,35 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
-    id("org.jetbrains.compose") version "1.1.1"
+    id("org.jetbrains.compose") version "1.1.0-alpha05"
 }
 
 group = "me.dinos"
-version = "1.6"
+version = "1.0.5"
+
+/*
+version = "1.0.3" working on Windows 7
+kotlin("jvm") version "1.5.31"
+id("org.jetbrains.compose") version "1.0.0"
+kotlinOptions.jvmTarget = "15"
+implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+includeAllModules = true
+
+version = "1.0.4"
+kotlin("jvm") version "1.6.10"
+id("org.jetbrains.compose") version "1.1.1"
+kotlinOptions.jvmTarget = "15"
+implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+includeAllModules = true
+
+version = "1.0.5"
+kotlin("jvm") version "1.6.10"
+id("org.jetbrains.compose") version "1.1.0-alpha05"
+kotlinOptions.jvmTarget = "15"
+implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+includeAllModules = true
+ */
+
 
 repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
@@ -20,21 +44,11 @@ repositories {
 dependencies {
     implementation(compose.desktop.currentOs)
     implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.2.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
 }
 
-// Add ProGuard to buildscript classpath
-//buildscript {
-//    repositories {
-//        mavenCentral()
-//    }
-//    dependencies {
-//        classpath("com.guardsquare:proguard-gradle:7.2.0")
-//    }
-//}
-
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "15"
     kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
 }
 
@@ -44,38 +58,12 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Msi)//for windows only
             packageName = "CSVEd"
-            packageVersion = "1.0.3"
+            packageVersion = "1.0.0"
             description = "CSV editor for Radio Mobile csv files"
             copyright = "©2022 Dinos Michelis. All rights reserved."
-//            val iconsRoot = project.file("src/main/resources")
-//            windows.iconFile.set(iconsRoot.resolve("icon.ico"))
             //modules to include
-            //includeAllModules = true
-            modules("java.instrument", "java.prefs", "jdk.unsupported")
+            includeAllModules = true
+            //modules("java.instrument", "java.prefs", "jdk.unsupported")
         }
     }
 }
-
-//tasks.jar {
-//    manifest {
-//        attributes["Main-Class"] = "MainKt"
-//    }
-//    configurations["compileClasspath"].forEach { file: File ->
-//        from(zipTree(file.absoluteFile))
-//    }
-//    duplicatesStrategy = DuplicatesStrategy.INCLUDE
-//}
-
-// Define task to obfuscate the JAR and output to <name>.min.jar
-//tasks.register<proguard.gradle.ProGuardTask>("obfuscate") {
-//    val packageUberJarForCurrentOS by tasks.getting
-//    dependsOn(packageUberJarForCurrentOS)
-//    val files = packageUberJarForCurrentOS.outputs.files
-//    injars(files)
-//    outjars(files.map { file -> File(file.parentFile, "${file.nameWithoutExtension}.min.jar") })
-//
-//    val library = if (System.getProperty("java.version").startsWith("1.")) "lib/rt.jar" else "jmods"
-//    libraryjars("${System.getProperty("java.home")}/$library")
-//
-//    configuration("proguard-rules.pro")
-//}
