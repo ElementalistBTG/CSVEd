b1a4eb80f465a3e27ba9b6d6f187eb2f7095ffd4
ElementalistBTG <elementalistbtg@gmail.com>
2023-10-07 23:39:17 +0300

    build commenting

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,26 +3,30 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    id("org.jetbrains.compose") version "1.1.1"
+    kotlin("jvm") version "1.6.10"//stable version 1.6.10
+    id("org.jetbrains.compose") version "1.1.1"//stable version 1.1.1
 }
 
 group = "me.dinos"
-version = "1.0.9"
+version = "1.0.10"
 
 repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     mavenCentral()
     mavenLocal()
     google()
+    maven {
+        url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        name = "Compose for Desktop DEV"
+    }
 }
 
 dependencies {
     implementation(compose.desktop.currentOs)
-    implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.2.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
-    implementation("org.jetbrains.skiko:skiko-awt:0.7.22")
-    implementation("org.jetbrains.skiko:skiko-awt-runtime-windows-x64:0.7.22")
+    implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.2.0")//stable version 1.2.0 (readNext method is deprecated after this version)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")//stable version 1.6.2, latest 1.7.3
+    implementation("org.jetbrains.skiko:skiko-awt:0.7.22") //stable version 0.7.22 (last to work with koltin 1.6), latest 0.7.81
+    implementation("org.jetbrains.skiko:skiko-awt-runtime-windows-x64:0.7.22")//stable version 0.7.22
 }
 
 tasks.withType<KotlinCompile> {
